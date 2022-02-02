32bc104bffce97e1007777740eb97596c7444a11
Richard Nguyen <laetic@users.noreply.github.com>
2022-02-02 09:34:48 -0700

    Switched keyboard to flexboxes and quickly styled

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,7 +1,9 @@
 import React from "react"
+import {nanoid} from 'nanoid'
 
 export default function Keyboard () {
-    const letters = "qwertyuiopasdfgjklzxcvbnm"
+    const letterRows = ["qwertyuiop","asdfgjkl","zxcvbnm"]
+
     function Key (props) {
         return (
             <button className="keyboard--key">
@@ -10,9 +12,17 @@ export default function Keyboard () {
         )
     }
 
+    function KeyboardRow (props) {
+        return (
+            <div className="keyboard--row">
+                {[...props.letters].map((letter) => <Key char={letter} key={nanoid()}/>)}
+            </div>
+        )
+    }
+
     return (
         <div className="keyboard">
-            {[...letters].map((letter) => <Key char={letter}/>)}
+            {[...letterRows].map((row) => <KeyboardRow letters={row} key={nanoid()}/>)}
         </div>
     )
     }
