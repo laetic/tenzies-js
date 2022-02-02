a8c33cf8d1d724fc4f8152250b5ae5af1b71f80f
Richard Nguyen <laetic@users.noreply.github.com>
2022-02-01 19:50:02 -0700

    Making a wordle branch

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
 
     return (
         <main>
-            <h1> Tenzies </h1>
+            <h1> Wordle </h1>
             <p> Roll until all the dice are the same. Click each die to freeze it at its current value between rolls</p>
             <div className="dice">
                 {dieElements}
