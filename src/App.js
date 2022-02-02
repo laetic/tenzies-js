357d72b7a41b37caaf78e9fa586f5d4596b2586d
Richard Nguyen <laetic@users.noreply.github.com>
2022-02-02 12:56:58 -0700

    Added setLetter, but using grid as state is bad

--- src/App.js
+++ src/App.js
@@ -4,22 +4,54 @@ import {nanoid} from 'nanoid'
 import Confetti from "react-confetti"
 import Keyboard from "./Keyboard"
 import Grid from "./Grid"
+import { fireEvent } from "@testing-library/react"
 
 export default function App() {
 
     const square = {letter: '', color:'white'}
     const row = {squares: [], letters: '', squareNum: 5, player:false};
-    const rowLetters = ['cdcdcdcd','cdcdcdcd','cdcdcdcd','cdcdcdcd','cdcdcdcd','cdcdcdcd']
-    const grid = {rows: [], rowLetters: rowLetters}
-
+    const [grid, setGrid] = React.useState({rows: [], rowLetters: ['abcde','cdcdcdcd','cdcdcdcd','cdcdcdcd','cdcdcdcd','cdcdcdcd']})
 
     for(let i = 0; i < row.squareNum; i++) {
-        row.squares.push(square);
+        row.squares.push({...square});
     }
 
     for (let i = 0; i < 6; i++) {
-        grid.rows.push(row);
+        grid.rows.push({...row});
+        if(i == 0) 
+            grid.rows[i].player = true;
     }
+
+    console.log(grid)
+        
+
+    function setLetter(letter) {
+        for (let i = 0; i < grid.rows.length; i++ ) {
+            if (grid.rows[i]) {
+                setGrid((prevGrid) => {
+                    return {...prevGrid, 
+                    rowLetters: prevGrid.rowLetters.map(
+                        (row) => row.player ? 
+                        row.push(letter) : 
+                        row)}})
+            }
+        }
+    }
+
+    function removeLetter(letter) {
+        for (let i = 0; i < grid.rows.length; i++ ) {
+            if (grid.rows[i]) {
+                setGrid((prevGrid) => {
+                    return {...prevGrid, 
+                    rowLetters: prevGrid.rowLetters.map(
+                        (row) => row.player ? 
+                        row.push(letter) : 
+                        row)}})
+            }
+        }
+    }
+
+
 
     return (
         <main>
@@ -28,7 +60,7 @@ export default function App() {
             <div className="grid">
                 {<Grid grid={grid}/>}
             </div>
-            <Keyboard />
+            <Keyboard setLetter={setLetter}/>
         </main>
     )
 }
